d2a248702f26f77716e78001ff9828255598a403
taiphan-fcs <tai.phan@floatingcube.com>
2025-04-17 16:54:47 +0700

    fix pick up time

--- assets/web/js/trip_booking_form.js
+++ assets/web/js/trip_booking_form.js
@@ -254,18 +254,19 @@ const $hourSelect = $("#ete_hour");
 const $minuteSelect = $("#ete_minute");
 const $pickUphourSelect = $("#pick_up_hour");
 const $pickUpminuteSelect = $("#pick_up_minute");
+const d = new Date();
+let current_hour = d.getHours();
+let current_date = d.getDate();
+let pickup_time = 0;
 
 
-for (let i = 0; i <= 23; i++) {
-  $hourSelect.append(`<option value="${i.toString().padStart(2, "0")}">${i.toString().padStart(2, "0")}</option>`);
-  $pickUphourSelect.append(`<option value="${i.toString().padStart(2, "0")}">${i.toString().padStart(2, "0")}</option>`);
-}
+parse_pickup_hour(current_date)
+
 
 for (let i = 0; i < 60; i += 5) {
   const value = i.toString().padStart(2, "0");
   $minuteSelect.append(`<option value="${value}">${value}</option>`);
   $pickUpminuteSelect.append(`<option value="${value}">${value}</option>`);
-  
 }
 
 $('#ete_hour, #ete_minute').on('change', () => {
@@ -288,3 +289,24 @@ $('#pick_up_hour, #pick_up_minute').on('change', () => {
 });
 
 
+$("body").on("click", "#calendar .vc-date", function(e){
+  let active_date = $(this).find("button").text();
+  $pickUphourSelect.html("");
+  parse_pickup_hour(active_date)
+})
+
+
+function parse_pickup_hour(active_date){
+  const d = new Date();
+  let current_date = d.getDate();
+  let start_hour = (active_date == current_date) ? parseInt(current_hour) + 1 : 0;
+  for (let i = start_hour; i <= 23; i++) {
+    let pickup_time = i.toString().padStart(2, "0"),
+        selected = "";
+    if(i == start_hour){
+      $pickUphourSelect.val(start_hour) ;
+      selected = "selected";
+    }
+    $pickUphourSelect.append(`<option ${selected} value="${pickup_time}">${pickup_time}</option>`);
+  }
+}
