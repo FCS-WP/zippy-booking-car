4d83dd1e653eda482f3c280ce3d2aa0940913709
taiphan-fcs <tai.phan@floatingcube.com>
2025-05-20 09:09:02 +0700

    [Woo Setting] Remove Extra Fee Tab

--- src/admin/zippy-woo-settings.php
+++ src/admin/zippy-woo-settings.php
@@ -16,50 +16,50 @@ class Zippy_Woo_Settings extends WC_Settings_Page
 {
   public function __construct()
   {
-    $this->id    = 'extra_fee_settings';
-    $this->label = 'Extra Fee Settings';
-    parent::__construct();
+    // $this->id    = 'extra_fee_settings';
+    // $this->label = 'Extra Fee Settings';
+    // parent::__construct();
   }
 
   public function get_settings()
   {
-    $settings = array(
-      array(
-        'title' => __('Extra Fee Settings', 'zippy-booking-car'),
-        'type'  => 'title',
-        'id'    => 'zippy_extra_fee_settings_title',
-      ),
-      array(
-        'name' => 'Enable/Disable',
-        'desc'     => __('Enable CC fee.', 'zippy-booking-car'),
-        'type' => 'checkbox',
-        'id' => 'enable_cc_fee',
-        'autoload' => true,
-      ),
-      array(
-        'title'    => __('CC Fee Amount', 'zippy-booking-car'),
-        'desc'     => __('Enter the default extra fee amount.', 'zippy-booking-car'),
-        'id'       => 'zippy_cc_fee_amount',
-        'default'  => '5',
-        'type'     => 'text',
-        'desc_tip' => true,
-      ),
+    // $settings = array(
+    //   array(
+    //     'title' => __('Extra Fee Settings', 'zippy-booking-car'),
+    //     'type'  => 'title',
+    //     'id'    => 'zippy_extra_fee_settings_title',
+    //   ),
+    //   array(
+    //     'name' => 'Enable/Disable',
+    //     'desc'     => __('Enable CC fee.', 'zippy-booking-car'),
+    //     'type' => 'checkbox',
+    //     'id' => 'enable_cc_fee',
+    //     'autoload' => true,
+    //   ),
+    //   array(
+    //     'title'    => __('CC Fee Amount', 'zippy-booking-car'),
+    //     'desc'     => __('Enter the default extra fee amount.', 'zippy-booking-car'),
+    //     'id'       => 'zippy_cc_fee_amount',
+    //     'default'  => '5',
+    //     'type'     => 'text',
+    //     'desc_tip' => true,
+    //   ),
 
-      array(
-        'title'    => __('Title', 'zippy-booking-car'),
-        'desc'     => __('Fee name that the customer will see on your website ', 'zippy-booking-car'),
-        'id'       => 'zippy_cc_fee_name',
-        'default'  => '5% CC fee',
-        'type'     => 'text',
-        'desc_tip' => true,
-      ),
+    //   array(
+    //     'title'    => __('Title', 'zippy-booking-car'),
+    //     'desc'     => __('Fee name that the customer will see on your website ', 'zippy-booking-car'),
+    //     'id'       => 'zippy_cc_fee_name',
+    //     'default'  => '5% CC fee',
+    //     'type'     => 'text',
+    //     'desc_tip' => true,
+    //   ),
 
-      array(
-        'type' => 'sectionend',
-        'id'   => 'zippy_extra_fee_settings_title',
-      ),
-    );
+    //   array(
+    //     'type' => 'sectionend',
+    //     'id'   => 'zippy_extra_fee_settings_title',
+    //   ),
+    // );
 
-    return apply_filters('woocommerce_get_settings_' . $this->id, $settings);
+    // return apply_filters('woocommerce_get_settings_' . $this->id, $settings);
   }
 }
